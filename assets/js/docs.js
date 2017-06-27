018cfc9fc1ff171d9205da77d8010bedaee3c9a7
Alexander Heimbuch <github@heimbu.ch>
2017-06-27 18:54:54 +0200

    fix missing assets

--- assets/js/docs.js
+++
@@ -1,8 +0,0 @@
----
----
-
-{% include_relative _includes/zepto.min.js %}
-{% include_relative _includes/prism.min.js %}
-{% include_relative _includes/lunar.min.js %}
-{% include_relative _includes/search.js %}
-{% include_relative _includes/mobile-navigation.js %}
